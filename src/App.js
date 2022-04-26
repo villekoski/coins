09f21155df4e86805389ef4324534b718fb7c99f
Ville Koski <bgj395@myy.haaga-helia.fi>
2022-04-26 22:34:56 +0300

    comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 import './App.css';
 import Coin from './Coin';
-
+// teksti
 
 function App() {
 const [coins, setCoins] = useState([])
